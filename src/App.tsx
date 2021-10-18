40472a64321a4ef84e62caf52f88d9c02158e983
noletorious <naatorres@gmail.com>
2021-10-17 17:00:52 -0700

    Install storybook, organize intro and Button story

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+
+const App: React.FC = () => {
+  return <p>This is a Storybook project</p>;
+};
+
+export default App;
